6fd835c9160d52ba7588b3c0d33f0ee32d4b2bca
Tunnes <cauepani06@gmail.com>
2023-04-13 19:37:57 -0300

    Att

--- vite.config.ts
+++ vite.config.ts
@@ -7,5 +7,5 @@ export default defineConfig({
   server: {
     port: 3000
   },
-  base: "https://thetunnes.github.io/codeleap-test-frontend"
+  base: "https://thetunnes.github.io/codeleap-test-frontend/dist/"
 })
